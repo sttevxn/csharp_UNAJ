c45ad6d95914acd97365f3cba9e22a4a5bb04cdb
Stev <steven10022001@outlook.com>
2023-11-27 02:04:55 -0300

    2.0

---
+++ trabajo_integrador/trabajo_integrador/DecisionExcepcion.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: steven
+ * Fecha: 24/11/2023
+ * Hora: 03:41 a.m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace trabajo_integrador
+{
+	/// <summary>
+	/// Description of DecisionExcepcion.
+	/// </summary>
+	public class DecisionExcepcion : Exception  
+	{
+		public string motivo;
+		
+		public DecisionExcepcion(string motivo)
+		{
+			this.motivo = motivo;
+		}
+	}
+}
